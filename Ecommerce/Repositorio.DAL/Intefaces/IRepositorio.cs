1e9a7b0dda381f5b896d526857076057b3f87ec6
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 22:14:16 -0300

    Interface IRepositorio

---
+++ Ecommerce/Repositorio.DAL/Intefaces/IRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.DAL.Intefaces
+{
+    public interface IRepositorio<T> where T : class
+    {
+        void Adicionar(T entidade);
+        void Adicionar(IEnumerable<T> entidades);
+
+        T Buscar(int ID);
+        IEnumerable<T> BuscarTodos();
+        IEnumerable<T> Buscar(Expression<Func<T, bool>> predicato);
+
+        void Remover(T entidade);
+        void Remover(IEnumerable<T> entidades);
+    }
+}
