2d9642bcf824ea1e6f3da3bea43a302a2c689f42
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 22:15:50 -0300

    Removendo Using desnecessários na interface IRepositorio

--- Ecommerce/Repositorio.DAL/Intefaces/IRepositorio.cs
+++ Ecommerce/Repositorio.DAL/Intefaces/IRepositorio.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿#region --Using--
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+#endregion
 
 namespace Repositorio.DAL.Intefaces
 {
