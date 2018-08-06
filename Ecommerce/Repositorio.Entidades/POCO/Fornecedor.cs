4b5fb3243798673c604a436ff1d21343a4e39a4a
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 21:44:45 -0300

    Classe Fornecedor

---
+++ Ecommerce/Repositorio.Entidades/POCO/Fornecedor.cs
@@ -0,0 +1,15 @@
+﻿#region --Using--
+using System.Collections.Generic;
+#endregion
+
+namespace Repositorio.Entidades.POCO
+{
+    public class Fornecedor
+    {
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public IList<Produto> Produtos { get; set; }
+    }
+}
