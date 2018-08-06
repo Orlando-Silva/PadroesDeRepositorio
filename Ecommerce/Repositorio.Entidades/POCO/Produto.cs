cc524121ef133fd9a16acdede69c05c75ee5c761
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 21:42:58 -0300

    Classe Produto

---
+++ Ecommerce/Repositorio.Entidades/POCO/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace Repositorio.Entidades.POCO
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Quantidade { get; set; }
+
+    }
+}
