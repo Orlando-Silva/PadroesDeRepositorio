6a3c321f8a4bd63d4dcd64318eafb7bb3e3c08ac
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 21:49:06 -0300

    Configurações da Fluent API para Produto

---
+++ Ecommerce/Repositorio.Entidades/FluentAPI/ProdutoConfiguracoes.cs
@@ -0,0 +1,19 @@
+﻿#region --Using--
+using Repositorio.Entidades.POCO;
+using System.Data.Entity.ModelConfiguration;
+#endregion
+
+namespace Repositorio.Entidades.FluentAPI
+{
+    public class ProdutoConfiguracoes : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoConfiguracoes()
+        {
+            Property(_ => _.Nome)
+                .HasMaxLength(64);
+
+            Property(_ => _.FornecedorID)
+                .HasColumnName("FornecedorID");
+        }
+    }
+}
