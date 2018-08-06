d0ec7c832ee56e4771dd17163b775f91b61dca77
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 22:49:50 -0300

    Implementação da IUnidadeDeTrabalho

---
+++ Ecommerce/Repositorio.DAL/UnidadeDeTrabalho.cs
@@ -0,0 +1,29 @@
+﻿#region --Using--
+using Repositorio.DAL.Contexto;
+using Repositorio.DAL.Intefaces;
+using Repositorio.DAL.Repositorios;
+#endregion
+
+namespace Repositorio.DAL
+{
+    public class UnidadeDeTrabalho : IUnidadeDeTrabalho
+    {
+        private readonly EcommerceContexto _contexto;
+
+        public UnidadeDeTrabalho(EcommerceContexto contexto)
+        {
+            _contexto = contexto;
+            Fornecedores = new FornecedorRepositorio(_contexto);
+            Produtos = new ProdutoRepositorio(_contexto);
+        }
+
+        public IFornecedorRepositorio Fornecedores { get; private set; }
+
+        public IProdutoRepositorio Produtos { get; private set; }
+
+        public void Finalizar() => _contexto.SaveChanges();
+
+        public void Dispose() => _contexto.Dispose();
+
+    }
+}
