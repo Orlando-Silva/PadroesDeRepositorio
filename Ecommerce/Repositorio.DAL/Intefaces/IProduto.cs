83950043e4e2077f6742aa531397d5a0df01e582
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 22:26:51 -0300

    IProduto

---
+++ Ecommerce/Repositorio.DAL/Intefaces/IProduto.cs
@@ -0,0 +1,15 @@
+﻿#region --Using--
+using Repositorio.Entidades.POCO;
+using System.Collections.Generic;
+#endregion
+
+namespace Repositorio.DAL.Intefaces
+{
+    public interface IProduto : IRepositorio<Produto> 
+    {
+        Produto BuscarProdutoComMaiorQuantidade();
+
+        IEnumerable<Produto> ProdutosSemQuantidade();
+
+    }
+}
