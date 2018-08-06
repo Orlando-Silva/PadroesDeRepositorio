4ea4449e674dff0400a9842cd482f2a80ac0d44e
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 22:35:49 -0300

    IFornecedorRepositorio

---
+++ Ecommerce/Repositorio.DAL/Intefaces/IFornecedorRepositorio.cs
@@ -0,0 +1,16 @@
+﻿#region --Using--
+using Repositorio.Entidades.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace Repositorio.DAL.Intefaces
+{
+    public interface IFornecedorRepositorio : IRepositorio<Fornecedor>
+    {
+        IEnumerable<Fornecedor> BuscarFornecedoresPorProduto(int produtoID);
+    }
+}
