7cdc2a1881f6a487c8f87c134fc4e4037271a497
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 22:36:16 -0300

    Removendo Using desnecessários na IFornecedorRepositorio

--- Ecommerce/Repositorio.DAL/Intefaces/IFornecedorRepositorio.cs
+++ Ecommerce/Repositorio.DAL/Intefaces/IFornecedorRepositorio.cs
@@ -1,10 +1,6 @@
 ﻿#region --Using--
 using Repositorio.Entidades.POCO;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 #endregion
 
 namespace Repositorio.DAL.Intefaces
