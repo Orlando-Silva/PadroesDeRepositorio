43649064898c25c71751975f8270c7a462af686e
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 22:45:01 -0300

    IUnidadeDeTrabalho

---
+++ Ecommerce/Repositorio.DAL/Intefaces/IUnidadeDeTrabalho.cs
@@ -0,0 +1,13 @@
+﻿#region --Using--
+using System;
+#endregion
+
+namespace Repositorio.DAL.Intefaces
+{
+    public interface IUnidadeDeTrabalho : IDisposable
+    {
+        IFornecedorRepositorio Fornecedores { get; }
+        IProdutoRepositorio Produtos { get; }
+        void Finalizar();
+    }
+}
