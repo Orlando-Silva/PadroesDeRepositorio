352fadf65291a2e7597b8b5d00f991bf26f5a35c
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-08-05 22:34:03 -0300

    ProdutoRepositorio

--- Ecommerce/Repositorio.DAL/Intefaces/IProdutoRepositorio.cs
+++ Ecommerce/Repositorio.DAL/Intefaces/IProdutoRepositorio.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace Repositorio.DAL.Intefaces
 {
-    public interface IProduto : IRepositorio<Produto> 
+    public interface IProdutoRepositorio : IRepositorio<Produto> 
     {
         Produto BuscarProdutoComMaiorQuantidade();
 
---
+++ Ecommerce/Repositorio.DAL/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,27 @@
+﻿#region --Using--
+using Repositorio.DAL.Contexto;
+using Repositorio.DAL.Intefaces;
+using Repositorio.Entidades.POCO;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace Repositorio.DAL.Repositorios
+{
+    public class ProdutoRepositorio : Repositorio<Produto>, IProdutoRepositorio
+    {
+
+        #region --Construtor--
+        public ProdutoRepositorio(EcommerceContexto contexto)
+            : base(contexto)
+        {
+
+        }
+        #endregion
+
+        public Produto BuscarProdutoComMaiorQuantidade() => Entidades.OrderByDescending(_ => _.Quantidade).FirstOrDefault();
+
+        public IEnumerable<Produto> ProdutosSemQuantidade() => Entidades.Where(_ => _.Quantidade == 0);
+
+    }
+}
